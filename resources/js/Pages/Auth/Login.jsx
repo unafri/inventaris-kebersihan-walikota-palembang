a8d3254e96583f6b07961be1e4667f657c351366
fannnn <82683291+unafri@users.noreply.github.com>
2025-11-25 11:38:58 +0700

    Implement password visibility toggle in Login form
    Added a toggle button to show/hide password input.

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -5,6 +5,7 @@ import PrimaryButton from "@/Components/PrimaryButton";
 import TextInput from "@/Components/TextInput";
 import GuestLayout from "@/Layouts/GuestLayout";
 import { Head, Link, useForm } from "@inertiajs/react";
+import React, { useState } from 'react';
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
@@ -12,6 +13,8 @@ export default function Login({ status, canResetPassword }) {
         password: "",
         remember: false,
     });
+
+    const [showPassword, setShowPassword] = useState(false);
 
     const submit = (e) => {
         e.preventDefault();
@@ -60,16 +63,32 @@ export default function Login({ status, canResetPassword }) {
                 <div className="mt-4">
                     <InputLabel htmlFor="password" value="Password" />
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        placeholder="Masukkan Password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData("password", e.target.value)}
-                    />
+                    <div className="relative mt-1">
+                        <TextInput
+                            id="password"
+                            type={showPassword ? "text" : "password"}
+                            name="password"
+                            placeholder="Masukkan Password"
+                            value={data.password}
+                            className="mt-1 block w-full pe-10"
+                            autoComplete="current-password"
+                            onChange={(e) => setData("password", e.target.value)}
+                        />
+
+                        <button
+                            type="button"
+                            className="absolute right-0 inset-y-0 flex items-center pr-3 text-gray-500 hover:text-gray-700"
+                            onClick={() => setShowPassword(!showPassword)}
+                        >
+                        {showPassword ? 
+                        <span className="material-symbols-outlined text-xl">
+                            visibility_off
+                        </span> : 
+                        <span className="material-symbols-outlined text-xl">
+                            visibility
+                        </span>}
+                        </button>
+                    </div>
 
                     <InputError message={errors.password} className="mt-2" />
                 </div>
@@ -98,15 +117,26 @@ export default function Login({ status, canResetPassword }) {
                     </PrimaryButton>
                 </div>
 
-                <div className="mt-2">
-                    {canResetPassword && (
+                <div className="flex justify-between mt-2">
+                    <div className="">
+                        {canResetPassword && (
+                            <Link
+                                href={route("password.request")}
+                                className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
+                                Forgot your password?
+                            </Link>
+                        )}
+                    </div>
+                        
+                    <div>
                         <Link
-                            href={route("password.request")}
+                            href={route("register")}
                             className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         >
-                            Forgot your password?
+                            Buat akun?
                         </Link>
-                    )}
+                    </div>
                 </div>
             </form>
         </GuestLayout>
